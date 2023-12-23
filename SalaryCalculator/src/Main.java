e91a842cabd5f4c200097e62e93e4828a6bea34e
sengir92 <onursengir92@gmail.com>
2023-12-24 01:15:33 +0300

    Refactor Main class

--- SalaryCalculator/src/Main.java
+++ SalaryCalculator/src/Main.java
@@ -5,15 +5,6 @@ public class Main {
         Employee e1 = new Employee("Kemal", 2000.0, 45, 1985);
 
 
-        //call the tax method to use
-        e1.tax();
-
-        //call the bonus method to use
-        e1.bonus();
-
-        //call the raiseSalary method to use
-        e1.raiseSalary();
-
         //call the toString method to use
         System.out.println(e1.toString());
 
